4db04d4c1ad43f46dabaf2161f91d712e32d606f
Hritik Bhandari <bhandari.hritik@gmail.com>
2020-07-03 03:16:11 +0530

    initial files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import './App.css';
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+
+    const fetchItems = async() => {
+      const result = await axios('https://www.breakingbadapi.com/api/characters')
+
+      console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+
+  }, [])
+
+
+  return (
+    <div className="container">
+      <Header/>
+      <CharacterGrid isLoading={isLoading} items={items} />
+      
+    </div>
+  );
+}
+
+export default App;
